597617cd2bb3dc930555cc8b5e177d0b6ed40a66
pfnegrini <pfnegrini@gmail.com>
2014-12-22 21:54:10 +0100

    Updated RPi-init.sh

--- RPi-init.sh
+++ RPi-init.sh
@@ -246,8 +246,8 @@ then
     mkdir /home/pi/Documents/Sketches
     mkdir /home/pi/Documents/Sketches/Bailey
     cd /home/pi/Documents/Sketches/Bailey
-    git clone https://github.com/pfnegrini/Bailey.git /home/pi/Documents/Sketches
-    sudo cp /home/pi/Documents/Sketches/server/install/robotserver /etc/init.d/robotserver
+    sudo git clone https://github.com/pfnegrini/Bailey.git /home/pi/Documents/Sketches/Bailey
+    sudo cp /home/pi/Documents/Sketches/Bailey/server/install/robotserver /etc/init.d/robotserver
     sudo chmod 0755 /etc/init.d/robotserver
     sudo update-rc.d robotserver defaults
 else
