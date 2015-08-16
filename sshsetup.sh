7bda1173d6363b5047bec4be69272d2ee042ede7
pfnegrini <pfnegrini@gmail.com>
2015-08-16 18:08:43 +0200

    Added script to simplify secure keys exchange

---
+++ sshsetup.sh
@@ -0,0 +1,23 @@
+#paolo@Goliath
+
+#cat ~/.ssh/id_rsa.pub | ssh pi@192.168.1.145 'cat >> .ssh/authorized_keys'
+
+NAME="N/A"
+SERVER="N/A"
+
+read -p "<YOURNANME>@<YOURDEVICE>? [$NAME]: " -e t1
+if [ -n "$t1" ]; then NAME="$t1";fi
+
+read -p "Server you wish to connect [$SERVER]: " -e t1
+if [ -n "$t1" ]; then SERVER="$t1";fi
+
+#list all installed packages: dpkg --get-selections
+#if [ "$STRIPALL" == "Y" ]
+#then
+#{
+#ssh-keygen -t rsa -C eben@pi
+#}
+
+
+ssh-keygen -t rsa -C $NAME
+cat ~/.ssh/id_rsa.pub | ssh $SERVER 'cat >> .ssh/authorized_keys'
